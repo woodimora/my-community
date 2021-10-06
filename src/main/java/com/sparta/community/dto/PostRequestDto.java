e2dd0d8dd323bfc880d4ebe9cb2663730fabac4f
woodimora <woodimora@gmail.com>
2021-10-07 02:05:55 +0900

    bean validation 추가

--- src/main/java/com/sparta/community/dto/PostRequestDto.java
+++ src/main/java/com/sparta/community/dto/PostRequestDto.java
@@ -2,8 +2,12 @@ package com.sparta.community.dto;
 
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 public class PostRequestDto {
+    @NotBlank(message = "게시글 제목을 입력하세요.")
     private String title;
+    @NotBlank(message = "게시글 내용을 입력하세요.")
     private String contents;
 }
